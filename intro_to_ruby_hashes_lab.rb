4c769401e8b68abf27d033e5dba4d9bd098a4a5e
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-20 21:30:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,15 @@ def monopoly_with_third_tier
     {
       :pieces => 4,
       :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
+        :reading_railroad => {
+          "mortgage_value" => "100"
+        },
+        :pennsylvania_railroad => {
+          "mortgage_value" => "200"
+        },
+        :b_and_o_railroad => {
+          "mortgage_value" => "300"
+        },
         :shortline => {}
       },
       :rent_in_dollars => {
