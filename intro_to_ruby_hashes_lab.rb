e8ceb0a8ec1dd74602f9a2d4069c4585c0b814bf
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-20 21:20:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,10 @@ def monopoly_with_third_tier
     {
       :pieces => 4,
       :names => {},
-      :rent_in_dollars => {}
+      :rent_in_dollars => {
+        :one_piece_owned => 25
+        
+      }
     }}
 end
 
